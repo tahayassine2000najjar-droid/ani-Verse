22ac7a6891c7588d945902de60730892806d66c9
tahayassine2000najjar-droid <tahayassine2000najjar@gmail.com>
2026-06-30 16:51:11 +0100

    Add NavBar component with navigation links

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,44 @@
+import {useState} from 'react'
+import {Link , useLocation} from 'react-router-dom'
+
+
+const links = [
+    {to: '/', label:'Home'},
+    {to: '/anime', label:'Anime'},
+    {to: '/characters', label:'Characters'},
+    {to: '/favorites', label:'Favorites'},
+    {to: '/my-library', label:'My Library'},
+    {to: '/dashboard', label:'Dashboard'},
+]
+export default function NavBar() {
+    const [open,setOpen] = useState(false)
+    const location = useLocation()
+
+      return (
+        <nav className="navbar">
+         <div className="navbar-inner container">
+            <Link to="/" className="navbar-brand">
+            <span className="brand-icon">&#9733;</span> AniVerse
+           </Link>
+           <button className="navbar-toggle" onClick={()=>setOpen(!open)}>
+            <span></span><span></span><span></span>
+           </button>
+           <ul className={`navbar-links ${open ? open :""}`}>
+            {links.map(link=>(
+              <li key={link.to}>
+                <Link
+                 to={link.to}
+                    className={location.pathname === Link.to? "active" : ""}
+                     onClick={()=>setOpen(false)}
+                     >
+                    {Link.label}
+                </Link>
+              </li>  
+              ))}
+           </ul>
+        </div>
+    </nav>
+  )
+}
+
+
